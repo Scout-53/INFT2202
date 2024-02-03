b4daa5a0e082c527b27f93de9a9e18d09b444281
Shan Patel <sspatel8457@gmail.com>
2024-02-02 21:27:30 -0500

    Inclass - 3. Most of it was done in class itself. Some changes in the HTML files were to me made so that the JavaScript could be implemented.

---
+++ ICE - 3/scripts/app.js
@@ -0,0 +1,63 @@
+// let boxes = $('.box');
+
+// boxes.each(function(index, box) {
+//     let currentBox = $(box).attr("class");
+
+//     if(currentClass === "box red-box"){
+//         $box.attr("class", "blue-box");
+//     }
+
+//     else if(currentClass === "box blue-box"){
+//         $box.attr("class", "green-box");
+//     }
+
+//     else if(currentClass === "box green-box"){
+//         $box.attr("class", "red-box");
+//     }
+
+//  });
+
+// let button = $('<button>Open MOdal</button>');
+
+// $('#content').append(button);
+
+// button.click(function() {
+//     $('lightbox').show();
+// });
+
+
+
+
+
+//change the class of each box, reordering the colours
+//get the boxes with jQuery
+let boxes = $('.box');
+//loop through each box
+boxes.each(function(index, box) {
+  //get the class of the current box
+  let currentClass = $(box).attr('class');
+  //if the class is red, make it blue
+  if (currentClass === 'box red-box') {
+    $(box).attr('class', 'blue-box');
+  }
+  //if the class is blue, make it green
+  else if (currentClass === 'box blue-box') {
+    $(box).attr('class', 'green-box');
+  }
+  //if the class is green, make it red
+  else if (currentClass === 'box green-box') {
+    $(box).attr('class', 'box red-box');
+  }
+});
+
+
+// create a button to open the modal
+let button = $('<button>Open Modal</button>');
+// add the button to the page
+$('#content').append(button);
+// add a click event listener to the button
+button.on('click', function() {
+  // show the modal
+  $('.lightbox').show();
+});
+
